11df495412cac8dccbf7b4accf66489ec6886a9b
JuanCarlosJ <93050072+juancjerezm@users.noreply.github.com>
2023-09-06 15:26:54 -0500

    Express

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -1,11 +1,34 @@
 const express = require("express");
+const ditto = require("./pokemon/ditto.json");
 
 const app = express();
-
+app.disable("x-powered-by");
 const PORT = process.env.PORT ?? 3000;
 
 app.get("/", (req, res) => {
-  res.status(200).send("<h1>Mi pagina</h1>");
+  res.send("<h1>Mi pagina</h1>");
+  // res.json({message: 'hola mundo'});
+});
+
+app.post("/pokemon", (req, res) => {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+  req.on("end", () => {
+    const data = JSON.parse(body);
+    data.timestamp = Date.now();
+    res.status(201).json(data);
+  });
+});
+
+app.get("/pokemon/ditto", (req, res) => {
+  res.json(ditto);
+});
+
+//el use, es para todas las opciones, es como si tuvieramos un asterisco
+app.use((req, res) => {
+  res.status(404).send("<h1>404</h1>");
 });
 
 app.listen(PORT, () => {
