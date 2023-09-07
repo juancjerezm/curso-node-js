3a8510bec0eb3ffff2a8f7774eb63ea073c55ecf
JuanCarlosJ <93050072+juancjerezm@users.noreply.github.com>
2023-09-06 22:25:29 -0500

    Fin ckase 2

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -3,7 +3,28 @@ const ditto = require("./pokemon/ditto.json");
 
 const app = express();
 app.disable("x-powered-by");
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3001;
+
+app.use(express.json());
+
+// app.use((req, res, next) => {
+//   if (req.method !== "POST") return next();
+//   if (req.headers["content-type"] !== "application/json") return next();
+//   //solo llegan request que son POST y que tienen el header Content-Type: application/json
+
+//   let body = "";
+
+//   //escuchar el evento
+//   req.on("data", (chunk) => {
+//     body += chunk.toString();
+//   });
+//   req.on("end", () => {
+//     const data = JSON.parse(body);
+//     data.timestamp = Date.now();
+//     req.body = data;
+//     next();
+//   });
+// });
 
 app.get("/", (req, res) => {
   res.send("<h1>Mi pagina</h1>");
@@ -11,15 +32,10 @@ app.get("/", (req, res) => {
 });
 
 app.post("/pokemon", (req, res) => {
-  let body = "";
-  req.on("data", (chunk) => {
-    body += chunk.toString();
-  });
-  req.on("end", () => {
-    const data = JSON.parse(body);
-    data.timestamp = Date.now();
-    res.status(201).json(data);
-  });
+  const data = req.body;
+  data.timestamp = Date.now();
+
+  res.status(201).json(req.body);
 });
 
 app.get("/pokemon/ditto", (req, res) => {
