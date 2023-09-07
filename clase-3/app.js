64891fc5ff0ab2e359a90fbd4d12b7c29d52a8b6
juancjerezm <juanchob61@gmail.com>
2023-09-07 18:36:49 -0500

    breack

--- clase-3/app.js
+++ clase-3/app.js
@@ -3,7 +3,12 @@ const express = require("express");
 const app = express();
 app.disable("x-powered-by");
 
-app.get("/", () => {
+app.get("/", (req, res) => {
   res.json({ message: "hola mundo" });
 });
 
+const PORT = process.env.PORT ?? 3001;
+
+app.listen(PORT, () => {
+  console.log(`server running on port http://localhost:${PORT}`);
+});
