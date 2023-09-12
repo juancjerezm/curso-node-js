a938c6845d33ba8ca015f6614ed09a4fb4208908
JuanCarlosJ <93050072+juancjerezm@users.noreply.github.com>
2023-09-11 19:57:29 -0500

    cambio a ESModules

--- clase-4/app.js
+++ clase-4/app.js
@@ -1,33 +1,41 @@
-const express = require('express') // require -> commonJS
-const crypto = require('node:crypto')
-const cors = require('cors')
+import express, { json } from "express"; // require -> commonJS
+import { randomUUID } from "node:crypto";
+import cors from "cors";
+import { readJSON } from "./utils.js";
 
-const movies = require('./movies.json')
-const { validateMovie, validatePartialMovie } = require('./schemas/movies')
+//Como leer un json en ESModules
+//import fs from 'node:fs'
+// const movies = JSON.parse(fs.readFileSync('./movies.json' , 'utf-8'))
 
-const app = express()
-app.use(express.json())
-app.use(cors({
-  origin: (origin, callback) => {
-    const ACCEPTED_ORIGINS = [
-      'http://localhost:8080',
-      'http://localhost:1234',
-      'https://movies.com',
-      'https://midu.dev'
-    ]
+//como leer un json en ESModules recomendado por ahora
+const movies = readJSON("./movies.json");
+import { validateMovie, validatePartialMovie } from "./schemas/movies.js";
 
-    if (ACCEPTED_ORIGINS.includes(origin)) {
-      return callback(null, true)
-    }
+const app = express();
+app.use(json());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        "http://localhost:8080",
+        "http://localhost:1234",
+        "https://movies.com",
+        "https://midu.dev",
+      ];
 
-    if (!origin) {
-      return callback(null, true)
-    }
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true);
+      }
 
-    return callback(new Error('Not allowed by CORS'))
-  }
-}))
-app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
+app.disable("x-powered-by"); // deshabilitar el header X-Powered-By: Express
 
 // métodos normales: GET/HEAD/POST
 // métodos complejos: PUT/PATCH/DELETE
@@ -36,84 +44,84 @@ app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
 // OPTIONS
 
 // Todos los recursos que sean MOVIES se identifica con /movies
-app.get('/movies', (req, res) => {
-  const { genre } = req.query
+app.get("/movies", (req, res) => {
+  const { genre } = req.query;
   if (genre) {
-    const filteredMovies = movies.filter(
-      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
-    )
-    return res.json(filteredMovies)
+    const filteredMovies = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    );
+    return res.json(filteredMovies);
   }
-  res.json(movies)
-})
+  res.json(movies);
+});
 
-app.get('/movies/:id', (req, res) => {
-  const { id } = req.params
-  const movie = movies.find(movie => movie.id === id)
-  if (movie) return res.json(movie)
-  res.status(404).json({ message: 'Movie not found' })
-})
+app.get("/movies/:id", (req, res) => {
+  const { id } = req.params;
+  const movie = movies.find((movie) => movie.id === id);
+  if (movie) return res.json(movie);
+  res.status(404).json({ message: "Movie not found" });
+});
 
-app.post('/movies', (req, res) => {
-  const result = validateMovie(req.body)
+app.post("/movies", (req, res) => {
+  const result = validateMovie(req.body);
 
   if (!result.success) {
     // 422 Unprocessable Entity
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
   }
 
   // en base de datos
   const newMovie = {
-    id: crypto.randomUUID(), // uuid v4
-    ...result.data
-  }
+    id: randomUUID(), // uuid v4
+    ...result.data,
+  };
 
   // Esto no sería REST, porque estamos guardando
   // el estado de la aplicación en memoria
-  movies.push(newMovie)
+  movies.push(newMovie);
 
-  res.status(201).json(newMovie)
-})
+  res.status(201).json(newMovie);
+});
 
-app.delete('/movies/:id', (req, res) => {
-  const { id } = req.params
-  const movieIndex = movies.findIndex(movie => movie.id === id)
+app.delete("/movies/:id", (req, res) => {
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
 
   if (movieIndex === -1) {
-    return res.status(404).json({ message: 'Movie not found' })
+    return res.status(404).json({ message: "Movie not found" });
   }
 
-  movies.splice(movieIndex, 1)
+  movies.splice(movieIndex, 1);
 
-  return res.json({ message: 'Movie deleted' })
-})
+  return res.json({ message: "Movie deleted" });
+});
 
-app.patch('/movies/:id', (req, res) => {
-  const result = validatePartialMovie(req.body)
+app.patch("/movies/:id", (req, res) => {
+  const result = validatePartialMovie(req.body);
 
   if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
   }
 
-  const { id } = req.params
-  const movieIndex = movies.findIndex(movie => movie.id === id)
+  const { id } = req.params;
+  const movieIndex = findIndex((movie) => movie.id === id);
 
   if (movieIndex === -1) {
-    return res.status(404).json({ message: 'Movie not found' })
+    return res.status(404).json({ message: "Movie not found" });
   }
 
   const updateMovie = {
     ...movies[movieIndex],
-    ...result.data
-  }
+    ...result.data,
+  };
 
-  movies[movieIndex] = updateMovie
+  movies[movieIndex] = updateMovie;
 
-  return res.json(updateMovie)
-})
+  return res.json(updateMovie);
+});
 
-const PORT = process.env.PORT ?? 1234
+const PORT = process.env.PORT ?? 1234;
 
 app.listen(PORT, () => {
-  console.log(`server listening on port http://localhost:${PORT}`)
-})
+  console.log(`server listening on port http://localhost:${PORT}`);
+});
--- clase-4/schemas/movies.js
+++ clase-4/schemas/movies.js
@@ -1,4 +1,4 @@
-const z = require("zod");
+import z from "zod";
 
 const movieSchema = z.object({
   title: z.string({
@@ -31,11 +31,10 @@ const movieSchema = z.object({
   ),
 });
 
-function validateMovie(object) {
+export function validateMovie(object) {
   return movieSchema.safeParse(object);
 }
 
-function validatePartialMovie(input) {
+export function validatePartialMovie(input) {
   return movieSchema.partial().safeParse(input);
 }
-module.exports = { validateMovie, validatePartialMovie };
