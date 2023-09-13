0a5289b0058f69ff419132861304907aa27e5205
JuanCarlosJ <93050072+juancjerezm@users.noreply.github.com>
2023-09-12 20:44:49 -0500

    creacion del modelo

---
+++ clase-4/models/movie.js
@@ -0,0 +1,35 @@
+import { readJSON } from "../utils";
+import { randomUUID } from "node:crypto";
+
+const movies = readJSON("../movies.json");
+
+export class MovieModel {
+  static async getAll({ genre }) {
+    if (genre) {
+      return movies.filter((movie) =>
+        movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+      );
+    }
+    return movies;
+  }
+  static async getById({ id }) {
+    const movie = movies.find((movie) => movie.id === id);
+    if (movie) return movie;
+  }
+
+  static async create(input) {
+    const newMovie = {
+      id: randomUUID(),
+      ...input,
+    };
+    movies.push(newMovie);
+    return newMovie;
+  }
+
+  static async delete({ id }) {
+    const movieIndex = movies.findIndex((movie) => movie.id === id);
+    if (!movieIndex) return false
+    movies.splice(movieIndex,1)
+    return true
+  }
+}
--- clase-4/routes/movies.js
+++ clase-4/routes/movies.js
@@ -2,52 +2,38 @@ import { Router } from "express";
 import { randomUUID } from "node:crypto";
 import { readJSON } from "../utils.js";
 import { validateMovie, validatePartialMovie } from "../schemas/movies.js";
+import { MovieModel } from "../models/movie.js";
+
 // import { createRequire } from "node:module";
 // const require = createRequire(import.meta.url);
 // export const readJSON = (path) => require(path);
 
 const movies = await readJSON("./movies.json");
 export const moviesRouter = Router();
-
-moviesRouter.get("/", (req, res) => {
+//------------------------------------------------
+moviesRouter.get("/", async (req, res) => {
   const { genre } = req.query;
-  if (genre) {
-    const filteredMovies = movies.filter((movie) =>
-      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
-    );
-    return res.json(filteredMovies);
-  }
+  const movies = await MovieModel.getAll({ genre });
   res.json(movies);
 });
-
-moviesRouter.get("/:id", (req, res) => {
+//-------------------------------------------------
+moviesRouter.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const movie = movies.find((movie) => movie.id === id);
+  const movie = await MovieModel.getById({ id });
   if (movie) return res.json(movie);
   res.status(404).json({ message: "Movie not found" });
 });
-
-moviesRouter.post("/", (req, res) => {
+//-----------------------------------------------
+moviesRouter.post("/", async (req, res) => {
   const result = validateMovie(req.body);
-
   if (!result.success) {
-    // 422 Unprocessable Entity
+    //422 Unprocesable Entity
     return res.status(400).json({ error: JSON.parse(result.error.message) });
   }
-
-  // en base de datos
-  const newMovie = {
-    id: randomUUID(), // uuid v4
-    ...result.data,
-  };
-
-  // Esto no sería REST, porque estamos guardando
-  // el estado de la aplicación en memoria
-  movies.push(newMovie);
-
+  const newMovie = await MovieModel.create(result.data);
   res.status(201).json(newMovie);
 });
-
+//-----------------------------------------------
 moviesRouter.delete("/:id", (req, res) => {
   const { id } = req.params;
   const movieIndex = movies.findIndex((movie) => movie.id === id);
